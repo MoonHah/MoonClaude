6a171a243b40636ae5854eb293dc34a9f97ed1ef
MoonHah <hh87@rice.edu>
2026-06-23 22:34:42 +0800

    S0: fix bug v0.0.1

--- src/moon_claude/core/transport/socket_server.py
+++ src/moon_claude/core/transport/socket_server.py
@@ -8,12 +8,12 @@ type CommandHandler = Callable[[dict[str, Any]], Awaitable[Any]]
 _MAX_LINE_BYTES = 1 * 1024 * 1024
 
 
-class SocketServerz:
+class SocketServer:
     def __init__(self, host: str, port: int) -> None:
         self._host = host
         self._port = port
         self._handlers: dict[str, CommandHandler] = {}
         self._server: asyncio.AbstractServer | None = None
 
-    def register(self, method: str, handler: CommandHandler):
+    def register(self, method: str, handler: CommandHandler) -> None:
         self._handlers[method] = handler
